5964aa38b44a625488f5a84d809a6da43332ad5f
datharnu <olagbemisoyee@gmail.com>
2024-11-18 03:54:19 +0800

    link

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,27 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
   return (
     <div className="lg:mx-10 mx-5 text-sm font-thin text-gray-400 lg:flex items-center gap-10 pb-10">
-      <a href="/" className="hover:text-blue-500 hover:underline">
+      <Link href="/" className="hover:text-blue-500 hover:underline">
         <h1>Privacy Policy</h1>
-      </a>
+      </Link>
 
-      <a href="/" className="hover:text-blue-500 hover:underline">
+      <Link href="/" className="hover:text-blue-500 hover:underline">
         <h1>Cookie Privacy</h1>
-      </a>
+      </Link>
 
-      <a href="/" className="hover:text-blue-500 hover:underline">
+      <Link href="/" className="hover:text-blue-500 hover:underline">
         <h1>Scouting Privacy</h1>
-      </a>
+      </Link>
 
-      <a href="/" className="hover:text-blue-500 hover:underline">
+      <Link href="/" className="hover:text-blue-500 hover:underline">
         <h1>Talent Guidelines and Resources</h1>
-      </a>
+      </Link>
 
-      <a href="/" className="hover:text-blue-500 hover:underline">
+      <Link href="/" className="hover:text-blue-500 hover:underline">
         <h1>Booking Conditions</h1>
-      </a>
+      </Link>
     </div>
   );
 }
